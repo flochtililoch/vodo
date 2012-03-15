10fc9d7c3667062a21b0f52f29e63705f147ed97
Florent <flochtililoch@gmail.com>
2012-03-15 14:27:24 +0100

    polishing code and interface

---
+++ web/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+  var callback = null,
+      feedContainer = $('.items'),
+      loading = $('.loading');
+  
+  // If url contains reference to an item, show overlay while loading items, then scroll to required item when loading is complete
+  if(window.location.hash)
+  {
+    loading.show();
+    callback = function() {
+      $((($.browser.chrome)||($.browser.safari)) ? document.body : document.documentElement)
+        .animate({ scrollTop: $(window.location.hash).offset().top }, 1000);
+      loading.hide();
+    }
+  }
+  
+  // Process feed
+  feedContainer.vodoFeed({
+    url: 'http://pipes.yahoo.com/pipes/pipe.run?_id=f42c711ab0e64056fd200b38ad98e102&_render=json&callback=myfunction',
+    callback: callback,
+    placeholder: $('.placeholder').remove().removeClass('placeholder')
+  });
+});
---
+++ web/js/vodo.js
@@ -0,0 +1,120 @@
+var vodo = (function($, self){
+  
+  function parseItem(placeholder, item) {
+    
+    // Parse item container
+    placeholder.parse({ data : item });
+
+    // Parse item content
+    placeholder.find('h2 a, img, p, span, em, .back').parse({ data : item });
+    placeholder.find('dd').parse({ data : item, replace: false });
+
+    // 'Action' links require tailored object depending on their [rel] attribute
+    var links = placeholder.find('.actions li');
+    $.each(item.link, function(k, link){
+      links
+        .filter('.' + link.rel)
+        .children('a')
+        .parse({ data : {
+          href: link.href,
+          title: item.title,
+          rights: item.rights,
+          duration: item.format.duration,
+          size: item.format.size
+        }});
+      });
+
+    return placeholder;
+  }
+  
+  self.loadFeed = function(o) {
+    $.getJSON(
+      o.url,
+      function(data) {
+        $.each(data.value.items, function(key, item){
+          
+          // Variables used to loop through items list
+          var index = 0,
+              itemsCount = item.entry.length;
+
+          // Do not freeze UI while processing list
+          var repeat = setInterval(function() {
+            
+            // Process and display item
+            o.container.append(
+              parseItem(
+                o.placeholder.clone(),
+                item.entry[index]
+                )
+              );
+
+            // Make sure there are still items to process
+            index++;
+            if(index >= itemsCount) {
+              clearInterval(repeat);
+              if (typeof o.callback === 'function') {
+                o.callback();
+              }
+            }
+          }, 150);
+        });
+      }
+    );
+  };
+  
+  // Hook into jQuery
+  $.fn.extend(
+	{
+		vodoFeed : function(o)
+		{
+			return this.each(
+				function()
+				{
+					// get selected element
+					var container = jQuery(this);
+					
+					// Handle click on info link
+          container.on("click", "a.info", function(event){
+          	$(this).parents('.item').toggleClass('flip');
+          });
+          
+          // Load feed into given element
+          self.loadFeed({
+					  url: o.url,
+					  container: container,
+					  placeholder: o.placeholder,
+					  callback: o.callback
+					});
+					
+					// Do not break jQuery chain
+					return container;
+				}
+			);
+		}
+	});
+  
+  return self;
+  
+})(jQuery, vodo || {});
+
+/* Vodo specific template filters */
+var tpl = (function(jQuery, self)
+{
+  // 'Normalize' date
+  function dateCleanup(date) {
+    // Date input format : YYYY-MM-DD-HH:II:SS xM
+    // Cross-browser date format for Date object : MM/DD/YYYY HH:II:SS xM (http://biostall.com/javascript-new-date-returning-nan-in-ie-or-invalid-date-in-safari) 
+    date = date.replace(/(\d{4})-(\d{2})-(\d{2})-(.*)/, '$2/$3/$1 $4');
+    return new Date(date);
+  };
+
+  // Convert date to human readable format
+  self.date = function(date) {
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    date = dateCleanup(date);
+    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+  };
+  
+  return self;
+})(jQuery, tpl || {});
